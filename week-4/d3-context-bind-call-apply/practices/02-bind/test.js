aec503b37a4f6e8be7f164a9adc3e305aaf04541
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-01-31 12:37:42 -0500

    first commit

--- week-4/d3-context-bind-call-apply/practices/02-bind/test.js
+++ week-4/d3-context-bind-call-apply/practices/02-bind/test.js
@@ -1,1 +1,15 @@
 // Your code here
+const Employee = require('./employee')
+
+const john = new Employee(
+  "John Wick",
+  "Dog Lover"
+)
+
+setTimeout(() => {
+  john.sayName()
+}, 2000)
+
+setTimeout(() => {
+  john.sayOccupation()
+}, 3000)
