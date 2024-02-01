c0c70d1820b79e15e828faa7db5b74c6aec1bcaf
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-02-01 13:09:03 -0500

    done errors

--- week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
+++ week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
@@ -1,26 +1,47 @@
 // 1.
 function sum(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+  try {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
+  } catch (error) {
+    if (error instanceof TypeError)
+    console.error(error.message)
   }
-  return sum;
 }
 
 let res = sum(null);
 console.log(res);
 
 // 2.
+
 // tests
 sayName("Alex");
 sayName(1);
 // Your code here
+function sayName(name){
+  try {
+    if (typeof name != "string")
+      throw new Error("Invalid name! Must be a string!")
+    console.log(name)
+  } catch (error) {
+    console.error(error.message)
+  }
+}
 
-// 3.
+// // 3.
 function greet(greeting) {
   if (!greeting) {
     throw new Error("There was no greeting given.");
   }
 
   console.log(greeting);
+}
+
+try {
+  greet()
+} catch (error) {
+  console.log("hello world")
 }
