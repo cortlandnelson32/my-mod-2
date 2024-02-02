2d5bfe34b7d7d887b4342fcaa0e243ddf49bb532
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-02-01 19:02:39 -0500

    testing

--- week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
@@ -1,13 +1,28 @@
 function isFive(num) {
   // Your code here
+  return num == 5 ? true : false
 }
 
 function isOdd(number) {
   // Your code here
+  if (typeof number === 'string') throw new Error("i am a string");
+  if (typeof number === 'object') throw new Error({ i: "am", an: "object" });
+  if (typeof number === 'array') throw new Error(["i", "am", "an", "array"]);
+
+  if (number % 2 !== 0){
+    return true
+  }
+  return false
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  let array = [];
+  for (let i = min; i <= max; i += step) {
+    array.push(i)
+  }
+  return array    
+
 }
 
 
--- week-4/d4-test-driven-development/practices/02-unit-testing/test/class-spec.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/test/class-spec.js
@@ -6,23 +6,34 @@ const { Word } = require("../class");
 describe("Word", function () {
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      // expect.fail("replace with your code");
+      const newWord = new Word('anything')
+      expect(newWord).to.have.property('word')
     });
 
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      // expect.fail("replace with your code");
+      const newWord = new Word('anything')
+      expect(newWord.word).to.equal('anything')
+      
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      // expect.fail("replace with your code");
+      const newWord = new Word('anything');
+      const result = newWord.removeVowels();
+      expect(result).to.equal('nythng');
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      // expect.fail("replace with your code");
+      const newWord = new Word('anything');
+      const result = newWord.removeConsonants();
+      expect(result).to.equal('ai');
     });
   });
 
