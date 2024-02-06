b44c6055d61901b2014619ee538229117f90a25d
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-02-06 16:20:46 -0500

    done dynamic arr

--- week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
+++ week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
@@ -3,17 +3,26 @@ class DynamicArray {
   constructor(defaultSize=4) {
 
     // Your code here
-
+    this.data = new Array(defaultSize),
+    this.capacity = defaultSize,
+    this.length = 0
   }
 
   read(index) {
-
     // Your code here
+    if (index >= this.length) return undefined;
+
+    return this.data[index]
   }
 
   unshift(val) {
-
     // Your code here
+    for (let i = this.length; i > 0; i++) {
+      this.data[i] = this.data[i - 1];
+      
+    }
+    this.data[0] = val
+    this.length++
   }
 
 }
