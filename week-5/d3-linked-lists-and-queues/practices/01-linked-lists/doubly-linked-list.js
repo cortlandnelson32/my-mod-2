dfb2b0ab781a8813c6a4c3a5a9b5a173e765d7e9
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-02-07 13:58:43 -0500

    done double

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
@@ -9,14 +9,47 @@ class DoublyLinkedListNode {
 class DoublyLinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
-  }
+    const newNode = new DoublyLinkedListNode(val);
+
+    // If the list is empty, set the new node as both head and tail
+    if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+    } else {
+        // If the list is not empty, add the new node to the head
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+    }
+
+    // Modify length
+    this.length++;
+}
 
   addToTail(val) {
     // Your code here
+    const newNode = new DoublyLinkedListNode(val);
+
+    if (!this.head){
+      this.head = newNode;
+      
+    } else {
+      let currentNode = this.head;
+
+      while(currentNode.next){
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+      newNode.prev = currentNode
+    }
+    this.tail = newNode
+    this.length++
   }
 
   // You can use this function to help debug
