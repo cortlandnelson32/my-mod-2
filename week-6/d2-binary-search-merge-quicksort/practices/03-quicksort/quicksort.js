daa681fe54036577ddefcca0a4a1aaddf51023a3
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-02-13 16:54:52 -0500

    done quick

--- week-6/d2-binary-search-merge-quicksort/practices/03-quicksort/quicksort.js
+++ week-6/d2-binary-search-merge-quicksort/practices/03-quicksort/quicksort.js
@@ -2,17 +2,33 @@ function quicksort(arr) {
 
   // Check if the input is length 1 or less
     // If so, it's already sorted: return
+  if (arr.length <= 1) return [...arr];
+
 
   // Pick the first value as the pivot
+  let piv = arr[0];
 
   // Orient the pivot so that...
       // every number smaller than the pivot is to the left
       // every number larger (or equal) than the pivot is to the right
+  let left = [];
+  let right = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < piv) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
 
   // Recursively sort the left
   // Recursively sort the right
+  left = quicksort(left);
+  right = quicksort(right);
 
-  // Return the left, pivot and right in sorted order
+  // Return the concatenation of left, pivot, and right in sorted order
+  return [...left, piv, ...right];
+  
 
 }
 
